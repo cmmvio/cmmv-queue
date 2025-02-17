1da65a8d5faada4b1728b23005bb7e2ac8d040a3
Andre <andre@tos.world>
2025-02-17 05:31:20 -0300

    refactor: eslint, package, update depedences

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+// Generated automatically by CMMV
+
+import 'reflect-metadata';
+import { Module, ApplicationTranspile } from '@cmmv/core';
+
+export let ApplicationModule = new Module('app', {
+    controllers: [],
+    providers: [],
+    transpilers: [ApplicationTranspile],
+});
